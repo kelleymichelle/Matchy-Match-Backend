3ce2fde1e002ebdf45883ebb2183d7f6faf4b572
Kelley Chaplain <kmchaplain@gmail.com>
2020-01-21 11:41:29 -0600

    deleted frontend folder and tested

--- frontend/scripts/easteregg.js
+++
@@ -1,7 +0,0 @@
-function projectMoonlight() {
-    const moon = document.getElementById('easter-egg')
-    moon.addEventListener('click', e => {
-      console.log(moon)
-      window.open('https://github.com/jamievullo/Moonlight', '_blank')
-    })
-}
